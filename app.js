6c31f497f9b0155f1266aa8621a2e22159f7ec30
nicktamburro <nicholastamburro@gmail.com>
2018-05-11 22:27:55 -0700

    fixed order issue!

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ $(document).ready(function(){
 
 var chapter = "STATELY, PLUMP BUCK MULLIGAN CAME FROM THE STAIRHEAD, bearing a bowl of lather on which a mirror and a razor lay crossed. A yellow dressing gown, ungirdled, was sustained gently-behind him by the mild morning air. He held the bowl aloft and intoned: Introibo ad altare Dei. Halted, he peered down the" 
 
-/*let words = chapter.split(" ");*/
+let words = chapter.split(" ");
+let picDiv;
 
-//for (i=0; i<words.length; i++){
+for (i=0; i<words.length; i++){
 	
-    //let word = words[i];
-
-    chapter.split(" ").reduce((p, word) => {
-    return p.then(() => {
+    let word = words[i];
+    let picDiv = $('<div/>');
+    picDiv.attr('id', word);
+    picDiv.append('<h1>' + word + '</h1>');
+    picDiv.appendTo($('#gifs-here'));
 
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + word + "&api_key=G3lLz1LwwD6VRySY0kEddhrhDoHrzGeX";
     $.ajax({
         url: queryURL,
         method: 'GET'
-        }).then(response => {
+        })
+        .done(function(response) {
+
                 var results = response.data;
 
-                    var gifDiv = $('<div/>');
+                    //var gifDiv = $('<div/>');
 
                     var gif = $('<img/>');
 
@@ -33,20 +37,15 @@ var chapter = "STATELY, PLUMP BUCK MULLIGAN CAME FROM THE STAIRHEAD, bearing a b
 
                     gif.attr('data-state', 'still');
 
-                    gifDiv.append(gif);
+                    picDiv.append(gif);
 
-                    gifDiv.append('<h1>' + word + '</h1>');
 
-                    gifDiv.appendTo($('#gifs-here'));
+                    //gifDiv.appendTo($('#gifs-here'));
 
   });
+    }
 });
-}, $.when())
-//return p
-});
-//chapter.split
 
-//document ready
 
 
 
