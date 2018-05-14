f70b402a22230288323483a4f93ae975402a9915
nicktamburro <nicholastamburro@gmail.com>
2018-05-13 23:04:07 -0700

    added more of the chapter, google fonts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var chapter = "STATELY, PLUMP BUCK MULLIGAN CAME FROM THE STAIRHEAD, bearing a bowl of lather on which a mirror and a razor lay crossed. A yellow dressing gown, ungirdled, was sustained gently-behind him by the mild morning air. He held the bowl aloft and intoned: Introibo ad altare Dei. Halted, he peered down the" 
+var chapter = "STATELY, PLUMP BUCK MULLIGAN CAME FROM THE STAIRHEAD, bearing a bowl of lather on which a mirror and a razor lay crossed. A yellow dressing gown, ungirdled, was sustained gently-behind him by the mild morning air. He held the bowl aloft and intoned: Introibo ad altare Dei. Halted, he peered down the dark winding stairs and called up coarsely" 
 
 let words = chapter.split(" ");
 let picDiv;
